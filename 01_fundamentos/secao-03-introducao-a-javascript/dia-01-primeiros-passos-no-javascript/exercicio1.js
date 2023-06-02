a75225e8f289a21aa60afbf3f5510be56d8c75cf
Rhenan Matias <rhenanmatias@gmail.com>
2023-06-01 22:22:51 -0300

    exercicio 1 e 2 concluídos

---
+++ 01_fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicio1.js
@@ -0,0 +1,14 @@
+/* Exercício 1
+Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. Escreva códigos para */
+
+let num1 = 7;
+let num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
+
+
