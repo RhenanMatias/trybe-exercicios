44063559528c03e234f7293d5ff645cdc4b21853
Rhenan Matias <rhenanmatias@gmail.com>
2023-06-06 10:32:28 -0300

    Atualização dos exercícios

--- 01_fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/array.js
+++ 01_fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/array.js
@@ -1,11 +1,60 @@
-/* Percorra o array imprimindo todos os valores contidos nele com a função console.log(). - OK
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-Some todos os valores contidos no array e imprima o resultado.
+// 1
 
-Calcule e imprima a média aritmética dos valores contidos no array. A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+console.log(numbers);
 
-Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+// 2
 
-Utilizando for, descubra o maior valor contido no array e imprima-o.
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  soma += numbers[index];
+}
 
-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”. */
+console.log(soma);
+
+// 3
+
+let media = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  media += numbers[index] / numbers.length;
+}
+
+console.log(media);
+
+// 4
+
+let resultadoMedia = "";
+if (media > 20) {
+  resultadoMedia = "O valor da média aritmética é maior que 20";
+} else {
+  resultadoMedia = "O valor da média aritmética é menor que 20";
+}
+
+console.log(resultadoMedia);
+
+// 5
+
+let maiorNum = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (maiorNum < numbers[index]) {
+    maiorNum = numbers[index];
+  }
+}
+
+console.log(maiorNum);
+
+// 6
+
+let countOdd = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 === 1) {
+    countOdd += 1;
+  }
+}
+if (countOdd > 0) {
+  countOdd = `Quantidade de números impares: ${countOdd}`;
+} else {
+  countOdd = "Nenhum valor ímpar encontrado";
+}
+console.log(countOdd);
