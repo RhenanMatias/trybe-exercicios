6316a4f481d624bed33209b66c0b85dc75688775
Rhenan Matias <rhenanmatias@gmail.com>
2023-07-08 20:09:31 -0300

    novaPessoaContratada

--- 01_fundamentos/secao-07-higher-order-function-do-javascript-es6/dia-01-introdução-a-higher-order-functions/part-01/novaPessoaContratada.js
+++ 01_fundamentos/secao-07-higher-order-function-do-javascript-es6/dia-01-introdução-a-higher-order-functions/part-01/novaPessoaContratada.js
@@ -3,3 +3,21 @@
 crie uma função que receba como parâmetro o nome completo da nova pessoa colaboradora e que automaticamente gere um email no formato nome_da_pessoa@trybe.com;
 
 utilize a função newEmployees, definida abaixo, para receber a sua função como parâmetro e criar o objeto de cada pessoa contratada.*/
+
+const generateEmail = (fullName) => {
+  const name = fullName.toLowerCase().replace(/ /g, "_");
+  // const name = fullName.toLowerCase().split(' ').join('_');
+  
+  return `${name}@trybe.com`;
+};
+
+const newEmployees = (generateEmail) => {
+  const employees = {
+    id1: generateEmail('Pedro Guerra'),
+    id2: generateEmail('Luiza Drumond'),
+    id3: generateEmail('Carla Paiva'),
+  }
+  return employees;
+};
+
+console.log(newEmployees(generateEmail))
